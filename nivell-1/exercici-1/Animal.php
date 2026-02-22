d4d79691e0a6e8e619a294503c94341e2f858e60
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-22 20:26:59 +0100

    feat: add Animal abstract class

---
+++ nivell-1/exercici-1/Animal.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class Animal {
+    protected string $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    abstract public function talk(): string;
+
+}
+
+?>
