658059ba0c16890e256b043beae787895330b1d9
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-23 13:05:28 +0100

    feat: implement AreaCalculator interface

--- nivell-2/Circle.php
+++ nivell-2/Circle.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once 'Shape.php';
+require_once 'AreaCalculator.php';
 
-class Circle extends Shape {
-
+class Circle implements AreaCalculator
+{
     private float $radius;
-    
-    public function __construct(float $radius) {
+
+    public function __construct(float $radius)
+    {
         $this->radius = $radius;
     }
 
-    public function area(): float {
+    public function calculateArea(): float
+    {
         return pi() * $this->radius * $this->radius;
     }
 }
-
-?>
