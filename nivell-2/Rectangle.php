724b5954c3bc453eb6adf39324442cef1c9e66c8
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-21 10:51:38 +0200

    feat: add Rectangle and Triangle classes implementing AreaCalculator interface

---
+++ nivell-2/Rectangle.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'AreaCalculator.php';
+
+class Rectangle implements AreaCalculator
+{
+    private float $base;
+    private float $height;
+
+    public function __construct(float $base, float $height)
+    {
+        $this->base = $base;
+        $this->height = $height;
+    }
+
+    public function calculateArea(): float
+    {
+        return $this->base * $this->height;
+    }
+}
