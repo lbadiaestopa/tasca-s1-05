646c143ca2b75774f7c18c6628f28d779cbe48f4
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-21 10:52:04 +0200

    feat: add Rectangle and Triangle

--- nivell-2/index.php
+++ nivell-2/index.php
@@ -1,6 +1,14 @@
 <?php
 
 require_once __DIR__ . '/Circle.php';
+require_once __DIR__ . '/Rectangle.php';
+require_once __DIR__ . '/Triangle.php';
 
 $circle = new Circle(12);
-echo $circle->calculateArea();
+echo $circle->calculateArea() . PHP_EOL;
+
+$rectangle = new Rectangle(12, 8);
+echo $rectangle->calculateArea() . PHP_EOL;
+
+$triangle = new Triangle(12, 8);
+echo $triangle->calculateArea();
